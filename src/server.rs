f44bb4a6569ef87694d4f232411809b61fefbf6f
Jared <againstlightning@gmail.com>
2021-06-09 10:51:29 -0400

    Enable stream read from connections

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use std::io::Read;
 use std::net::TcpListener;
 
 pub struct Server {
@@ -13,5 +14,22 @@ impl Server {
         println!("Listening on {}", self.address);
 
         let listener = TcpListener::bind(&self.address).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Request: {}", String::from_utf8_lossy(&buffer))
+                        }
+                        Err(e) => {
+                            println!("Stream read error: {}", e)
+                        }
+                    }
+                }
+                Err(e) => println!("Connection error: {}", e),
+            }
+        }
     }
 }
